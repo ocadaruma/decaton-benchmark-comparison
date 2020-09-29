133290c93b1ed29c6f1b3226af03e0a0b77c5fb1
ocadaruma <ocadaruma@gmail.com>
2020-09-29 17:16:41 +0900

    init

---
+++ src/main/java/com/mayreh/KafkaStreamsRunner.java
@@ -0,0 +1,47 @@
+package com.mayreh;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+
+import com.linecorp.decaton.benchmark.ProcessCallback;
+import com.linecorp.decaton.benchmark.ResourceTracker;
+import com.linecorp.decaton.benchmark.Runner;
+import com.linecorp.decaton.benchmark.Task;
+
+public class KafkaStreamsRunner implements Runner {
+    private KafkaStreams streams;
+
+    @Override
+    public void init(Config config, ProcessCallback callback, ResourceTracker resourceTracker)
+            throws InterruptedException {
+        Serializer<Task> taskSerializer = (topic, data) -> {
+            throw new UnsupportedOperationException("Never used");
+        };
+
+        StreamsBuilder builder = new StreamsBuilder();
+        builder.stream(config.topic(), Consumed.with(
+                Serdes.String(),
+                Serdes.serdeFrom(taskSerializer, config.taskDeserializer())))
+               .foreach((key, task) -> callback.process(task));
+
+        Properties props = new Properties();
+        props.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServers());
+        props.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "decaton-benchmark-kafkastreams");
+        config.parameters().forEach(props::setProperty);
+
+        streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
+
+    @Override
+    public void close() throws Exception {
+        streams.close();
+    }
+}
